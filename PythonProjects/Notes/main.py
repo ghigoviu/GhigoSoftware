46af966305941d111fe741cdd6fced00486789fd
ghigoviu <ghigoviu@gmail.com>
2021-10-10 00:52:52 -0500

    Avance importante de la tarea

---
+++ PythonProjects/Notes/main.py
@@ -0,0 +1,32 @@
+# This is a sample Python script.
+
+# Press Mayús+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+#Basic Notes
+
+#Comentarios...
+
+#Asignación de variables:
+entero = 1
+cadena = "Esta es una cadena"
+booleano = True
+
+#Impresión en pantalla:
+print(entero)
+print(cadena + entero)
+
+print(type(entero))
+
